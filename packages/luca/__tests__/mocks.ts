833b7b47fed1880cca85b8173af46fc1e280cad8
German Meza <13009536+germanamz@users.noreply.github.com>
2025-09-23 12:09:38 -0600

    context (#20)

---
+++ packages/luca/__tests__/mocks.ts
@@ -0,0 +1,41 @@
+import { Backend } from '@germanamz/luca-common';
+import Decimal from 'decimal.js';
+
+export const mockBackend = (props?: Partial<Backend>): Backend => ({
+  createTransaction: vi.fn().mockResolvedValue('transactionId'),
+  createEntry: vi.fn().mockResolvedValue('entryId'),
+  getAccount: vi.fn().mockResolvedValue({
+    id: 'accountId',
+    name: 'Account',
+    side: 'DEBIT',
+    credits: new Decimal(0),
+    debits: new Decimal(0),
+  }),
+  getTransaction: vi.fn().mockResolvedValue({
+    id: 'transactionId',
+    sourceAccountId: 'sourceAccountId',
+    destinationAccountId: 'destinationAccountId',
+    amount: new Decimal(0),
+    concept: 'Test Transaction',
+    date: new Date(),
+  }),
+  getEntry: vi.fn().mockResolvedValue({
+    id: 'entryId',
+    accountId: 'accountId',
+    transactionId: 'transactionId',
+    side: 'DEBIT',
+    amount: new Decimal(0),
+    date: new Date(),
+  }),
+  updateAccount: vi.fn(),
+  getAccountChildren: vi.fn().mockResolvedValue([
+    {
+      id: 'accountId',
+      name: 'Account',
+      side: 'DEBIT',
+      credits: new Decimal(0),
+      debits: new Decimal(0),
+    },
+  ]),
+  ...props,
+});
--- packages/luca/src/Luca.ts
+++ packages/luca/src/Luca.ts
@@ -1,4 +1,4 @@
-import { Backend, inverseSide } from '@germanamz/luca-common';
+import { Account, Backend, inverseSide } from '@germanamz/luca-common';
 import { CreateTransactionRequest, CreateEntryRequest } from './types';
 
 export class Luca {
@@ -8,19 +8,23 @@ export class Luca {
     this.backend = backend;
   }
 
-  async createEntry(request: CreateEntryRequest) {
+  async createEntry<C>(request: CreateEntryRequest, ctx?: C) {
     const { accountId, transactionId, side, amount, date } = request;
-    const entryId = await this.backend.createEntry({
-      accountId,
-      transactionId,
-      side,
-      amount,
-      date,
-    });
+    const entryId = await this.backend.createEntry(
+      {
+        accountId,
+        transactionId,
+        side,
+        amount,
+        date,
+      },
+      ctx,
+    );
     let accId: string | undefined = accountId;
 
     while (accId) {
-      let { credits, debits, parentId } = await this.backend.getAccount(accId);
+      let { credits, debits, parentId }: Account =
+        await this.backend.getAccount(accId, ctx);
 
       if (side === 'CREDIT') {
         credits = credits.add(amount);
@@ -28,10 +32,14 @@ export class Luca {
         debits = debits.add(amount);
       }
 
-      await this.backend.updateAccount(accId, {
-        credits,
-        debits,
-      });
+      await this.backend.updateAccount(
+        accId,
+        {
+          credits,
+          debits,
+        },
+        ctx,
+      );
 
       accId = parentId;
     }
@@ -39,44 +47,59 @@ export class Luca {
     return entryId;
   }
 
-  async createTransaction({
-    amount,
-    destinationAccountId,
-    sourceAccountId,
-    concept,
-    parentId,
-    date,
-  }: CreateTransactionRequest) {
-    const sourceAcc = await this.backend.getAccount(sourceAccountId);
-    const destinationAcc = await this.backend.getAccount(destinationAccountId);
-    const transactionId = await this.backend.createTransaction({
-      concept,
+  async createTransaction<C>(
+    {
       amount,
       destinationAccountId,
       sourceAccountId,
+      concept,
       parentId,
       date,
-    });
+    }: CreateTransactionRequest,
+    ctx?: C,
+  ) {
+    const sourceAcc = await this.backend.getAccount(sourceAccountId, ctx);
+    const destinationAcc = await this.backend.getAccount(
+      destinationAccountId,
+      ctx,
+    );
+    const transactionId = await this.backend.createTransaction(
+      {
+        concept,
+        amount,
+        destinationAccountId,
+        sourceAccountId,
+        parentId,
+        date,
+      },
+      ctx,
+    );
     const sameSideSrcAndDest = sourceAcc.side === destinationAcc.side;
     // If the source and destination accounts have the same side, this means that they are on the same side of the accounting equation,
     // therefore the source entry should be on the opposite side of the account's normal side (decrease the source account balance)
     const sourceSide = sameSideSrcAndDest
       ? inverseSide(sourceAcc.side)
       : sourceAcc.side;
-    const sourceEntryId = await this.createEntry({
-      accountId: sourceAccountId,
-      transactionId,
-      side: sourceSide,
-      amount,
-      date,
-    });
-    const destinationEntryId = await this.createEntry({
-      accountId: destinationAccountId,
-      transactionId,
-      side: destinationAcc.side,
-      amount,
-      date,
-    });
+    const sourceEntryId = await this.createEntry(
+      {
+        accountId: sourceAccountId,
+        transactionId,
+        side: sourceSide,
+        amount,
+        date,
+      },
+      ctx,
+    );
+    const destinationEntryId = await this.createEntry(
+      {
+        accountId: destinationAccountId,
+        transactionId,
+        side: destinationAcc.side,
+        amount,
+        date,
+      },
+      ctx,
+    );
 
     return {
       transactionId,
